4b76b01db35798f96926977313803ebc9651c5bd
NMGVox <nijeemgreene@gmail.com>
2023-06-09 14:48:28 -0400

    Finish implementation of travails method

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const gameBoard = (function() {
     spaces= [...Array(8)].map(e => Array(8));
 
     function isValidMove(newPos) {
-        if(newPos[0] > spaces.length || newPos[0] < 0) {
+        if(newPos[0] > spaces.length - 1 || newPos[0] < 0) {
             return false;
-        } else if ( newPos[1] > spaces[0].length || newPos[1] < 0 ) {
+        } else if ( newPos[1] > spaces[0].length - 1|| newPos[1] < 0 ) {
             return false;
         }
         return true;
     }
 
     return {
-        spaces
+        spaces,
+        isValidMove
     };
 })();
 
@@ -19,7 +20,7 @@ function knightFactory(start, end) {
     let initialPos = start;
     let endPos = end;
     let queue = [];
-    let moves = [[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,1]];
+    let moves = [[2,1], [1,2], [-2,1], [-1,2], [2,-1], [1,-2], [-2,-1], [-1,-2]];
 
     function addPossibleMove (node) {
         queue.push(node);
@@ -35,36 +36,54 @@ function knightFactory(start, end) {
         return node.visited.includes(newPos) ? false : true;
     }
 
+    function travailsHelper() {
+        let curr = nodeFactory(initialPos, []);
+        while( curr !== null ) {
+            if( curr.position[0] === endPos[0] &&  curr.position[1] === endPos[1]) {
+                console.log(curr);
+                return curr;
+            }
+            moves.forEach(move => {
+                let newPos = [(curr.position[0] + move[0]),(curr.position[1] + move[1])]
+                if(!gameBoard.isValidMove(newPos) || !wasNotVisited(curr, newPos)) {
+                    return;
+                }
+                let newNode = nodeFactory(newPos, curr.visited);
+                addPossibleMove(newNode); 
+            });
+            curr = nextNode();
+        }
+    }
+
+    function travails() {
+        let result = travailsHelper();
+        console.log(`You made it in ${result.visited.length - 1} moves! Check 'em out:`);
+        result.visited.forEach(space => {
+            console.log(space);
+        })
+    }
+
     return {
         initialPos,
         endPos,
         queue,
         moves,
         addPossibleMove,
-        wasNotVisited,
-        nextNode
+        nextNode,
+        travails
     }
 }
 
 function nodeFactory( curr, prev ) {
     let position = curr;
-    let visited = prev.append(position);
+    let visited = prev.concat([position]);
 
     return {
         position,
         visited,
-        wasNotVisited
     }
 }
 
-let newKnight = knightFactory([3,4]);
-newKnight.addPossibleMove([4,6]);
-newKnight.addPossibleMove([5,8]);
-newKnight.addPossibleMove([7,7]);
+let newKnight = knightFactory([3,3], [4,3]);
 
-console.log(newKnight.queue);
-
-newKnight.nextNode();
-newKnight.nextNode();
-
-console.log(newKnight.queue);
+newKnight.travails();
